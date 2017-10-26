fbd076069c70ea5a69cbb1ddcb43bde18af8d286
75u2u <75u2uuy4@gmail.com>
2017-10-26 22:12:05 +0900

    update

---
+++ 18F27J53/binary_watch.X/7seg.h
@@ -0,0 +1,19 @@
+
+#ifndef _7SEG_H
+#define	_7SEG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include <xc.h>
+#include <stdint.h>
+
+    void print_7seg(uint8_t col, char c);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* 7SEG_H */
+
